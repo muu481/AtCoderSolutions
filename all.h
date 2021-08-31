0c7ea855c30e7047fc0ace34ecddc8c0b4a4c270
muuchang <rnnmellav@gmail.com>
2021-09-01 00:16:25 +0900

    add all.h

---
+++ all.h
@@ -0,0 +1,24 @@
+#include <string>
+#include <array>
+#include <deque>
+#include <forward_list>
+#include <list>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <iterator>
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+#include <regex>
+#include <atomic>
+#include <thread>
+
+using namespace std;
+#define ARRAY_SIZE(array) (sizeof(array) / sizeof(array[0]))
